57ec47019b47e9fad510dd4a440b4c6fe28f7c3f
mcanueste <mcanueste@gmail.com>
2023-11-23 16:47:16 +0100

    add home row mode helpers

--- config/helpers.h
+++ config/helpers.h
@@ -1,8 +1,12 @@
 /* vim: set ft=c tw=174: */
 #pragma once
 
+// Global definitions
+#define QUICK_TAP_MS 175
+
 #define ZMK_HELPER_STRINGIFY(x) #x
 
+// Create ZMK behavior
 #define ZMK_BEHAVIOR(name, type, ...) \
     / { \
         behaviors { \
@@ -14,6 +18,7 @@
         }; \
     };
 
+// Create ZMK layer
 #define ZMK_LAYER(name, layout) \
     / { \
         keymap { \
@@ -25,3 +30,14 @@
         }; \
     };
 
+// Create Home Row Mode key maker
+#define MAKE_HRM(NAME, HOLD, TAP, TRIGGER_POS) \
+    ZMK_BEHAVIOR(NAME, hold_tap, \
+        flavor = "balanced"; \
+        tapping-term-ms = <280>; \
+        quick-tap-ms = <QUICK_TAP_MS>; \
+        require-prior-idle-ms = <150>; \
+        bindings = <HOLD>, <TAP>; \
+        hold-trigger-key-positions = <TRIGGER_POS>; \
+        hold-trigger-on-release; \
+    )
