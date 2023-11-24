b16d59ad3b79526c267c9c2638cbd8f6dee38437
mcanueste <mcanueste@gmail.com>
2023-11-24 19:04:42 +0100

    test layer taps

--- config/helpers.h
+++ config/helpers.h
@@ -3,6 +3,10 @@
 
 // Aliases
 #define QUICK_TAP_MS 175
+#define HOMEROW_TAPPING_TERM_MS 280
+#define THUMB_TAPPING_TERM_MS 200
+#define TMUX_PREFIX LA(A)
+
 #define ZMK_BEHAVIOR_CORE_caps_word       compatible = "zmk,behavior-caps-word";       #binding-cells = <0>
 #define ZMK_BEHAVIOR_CORE_dynamic_macro   compatible = "zmk,behavior-dynamic-macro";   #binding-cells = <1>
 #define ZMK_BEHAVIOR_CORE_hold_tap        compatible = "zmk,behavior-hold-tap";        #binding-cells = <2>
@@ -14,8 +18,6 @@
 #define ZMK_BEHAVIOR_CORE_sticky_key      compatible = "zmk,behavior-sticky-key";      #binding-cells = <1>
 #define ZMK_BEHAVIOR_CORE_tap_dance       compatible = "zmk,behavior-tap-dance";       #binding-cells = <0>
 #define ZMK_BEHAVIOR_CORE_tri_state       compatible = "zmk,behavior-tri-state";       #binding-cells = <0>
-#define TMUX_PREFIX LA(A)
-
 
 #define ZMK_HELPER_STRINGIFY(x) #x
 
@@ -47,13 +49,19 @@
 #define MAKE_HRM(NAME, HOLD, TAP, TRIGGER_POS) \
     ZMK_BEHAVIOR(NAME, hold_tap, \
         flavor = "balanced"; \
-        tapping-term-ms = <280>; \
+        tapping-term-ms = <HOMEROW_TAPPING_TERM_MS>; \
         quick-tap-ms = <QUICK_TAP_MS>; \
         require-prior-idle-ms = <150>; \
         bindings = <HOLD>, <TAP>; \
         hold-trigger-key-positions = <TRIGGER_POS>; \
         hold-trigger-on-release; \
     )
+
+&lt {  // layer-tap config
+    flavor = "balanced";
+    tapping-term-ms = <THUMB_TAPPING_TERM_MS>;
+    quick-tap-ms = <QUICK_TAP_MS>;
+};
 
 // Create TMUX combos
 // wait-ms = <0>;
