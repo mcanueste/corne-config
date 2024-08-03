88292ba003e5897c4168853af7f00afa172b6f39
mcanueste <mcanueste@gmail.com>
2024-08-03 21:05:54 +0200

    try homerow combos

--- config/helpers.h
+++ config/helpers.h
@@ -8,6 +8,7 @@
 #define PRIOR_IDLE_MS 100
 #define HOMEROW_TAPPING_TERM_MS 200
 #define THUMB_TAPPING_TERM_MS 180
+#define COMBO_TIMEOUT_MS 50
 
 #define ZMK_BEHAVIOR_CORE_caps_word       compatible = "zmk,behavior-caps-word";       #binding-cells = <0>
 #define ZMK_BEHAVIOR_CORE_dynamic_macro   compatible = "zmk,behavior-dynamic-macro";   #binding-cells = <1>
@@ -21,32 +22,41 @@
 #define ZMK_BEHAVIOR_CORE_tap_dance       compatible = "zmk,behavior-tap-dance";       #binding-cells = <0>
 #define ZMK_BEHAVIOR_CORE_tri_state       compatible = "zmk,behavior-tri-state";       #binding-cells = <0>
 
-// Create ZMK behavior
-#define ZMK_HELPER_STRINGIFY(x) #x
-#define ZMK_BEHAVIOR(name, type, ...) \
+#define ZMK_HELPER_STRINGIFY(X) #X
+
+// Create ZMK layer
+#define ZMK_LAYER(NAME, LAYOUT) \
+    / { \
+        keymap { \
+            compatible = "zmk,keymap"; \
+            layer_##NAME { \
+                label = ZMK_HELPER_STRINGIFY(NAME); \
+                bindings = <LAYOUT>; \
+            }; \
+        }; \
+    };
+
+// Combo Configuration
+#define COMBO(NAME, LAYER, BINDINGS, KEYPOS) \
+  combo_##NAME { \
+    timeout-ms = <50>; \
+    bindings = <BINDINGS>; \
+    key-positions = <KEYPOS>; \
+    layers = <LAYER>; \
+  };
+
+// Create Home Row Mode key maker
+#define ZMK_BEHAVIOR(NAME, TYPE, ...) \
     / { \
         behaviors { \
             name: name { \
-                label = ZMK_HELPER_STRINGIFY(ZB_ ## name); \
-                ZMK_BEHAVIOR_CORE_ ## type; \
+                label = ZMK_HELPER_STRINGIFY(ZB_##NAME); \
+                ZMK_BEHAVIOR_CORE_##TYPE; \
                 __VA_ARGS__ \
             }; \
         }; \
     };
 
-// Create ZMK layer
-#define ZMK_LAYER(name, layout) \
-    / { \
-        keymap { \
-            compatible = "zmk,keymap"; \
-            layer_ ## name { \
-                label = ZMK_HELPER_STRINGIFY(name); \
-                bindings = <layout>; \
-            }; \
-        }; \
-    };
-
-// Create Home Row Mode key maker
 #define MAKE_HRM(NAME, HOLD, TAP, TRIGGER_POS) \
     ZMK_BEHAVIOR(NAME, hold_tap, \
         flavor = "balanced"; \
@@ -66,7 +76,8 @@ MAKE_HRM(hmr, &kp, &kp, KEYS_L THUMBS)  // right-hand HRMs
 MAKE_HRM(hll, &mo, &kp, KEYS_R THUMBS)  // left-hand HRMs
 MAKE_HRM(hlr, &mo, &kp, KEYS_L THUMBS)  // right-hand HRMs
 
-&lt {  // layer-tap config
+// layer-tap config
+&lt {
     flavor = "balanced";
     tapping-term-ms = <THUMB_TAPPING_TERM_MS>;
     quick-tap-ms = <QUICK_TAP_MS>;
